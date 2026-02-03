1089288dee0feec64f9af05d0b951f4ee690edbf
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2026-02-03 17:54:58 +0800

    Add SubmissionForm route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import SubmissionForm from "./pages/SubmissionForm"
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/sub-form" element={<SubmissionForm/>} />
     </Routes>
   )
 }
