4f96473dfa1c9310254fd149b52844f0356f7364
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2026-02-06 20:28:23 +0800

    Update Home component to include link to Submission Form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
     return (
-        <h1>Home Page</h1>
+        <>
+        <Link to="/submission-form">Go to Submission Form</Link>
+        </>
     )
 }
